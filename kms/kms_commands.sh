d2eaf1175f06783395a6d5499b0df83ab7efb9e0
olupy <fortuneosho@gmail.com>
2024-07-29 21:16:59 +0100

    stash

--- kms/kms_commands.sh
+++ kms/kms_commands.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 TRANSACTION_URL="${TRANSACTION_URL:-https://apis.greyboxpay.com/api/v1/transaction/verify}"
 PERIOD_OF_CHECK="${PERIOD_OF_CHECK:-300}"
-WALLET_STORAGE_LOCATION="${WALLET_STORAGE_LOCATION:-/kms/wallet/wallet.dat}"
+WALLET_STORAGE_LOCATION="${WALLET_STORAGE_LOCATION:-$(pwd)/wallet/wallet.dat}"
 CHAINS="${CHAINS:-CELO,XLM}"
+ENVFILE="${ENVFILE:-$(pwd)/.env}"
+
+# Load environment variables from the specified .env file
 
 # Start the Tatum KMS daemon with specified configurations
-exec tatum-kms daemon \
+exec docker run -it --env-file .env -v $pwd:/root/.tatumrc tatumio/tatum-kms daemon \
   --external-url="$TRANSACTION_URL" \
   --period="$PERIOD_OF_CHECK" \
-  --path="$WALLET_STORAGE_LOCATION" \
-  --chain="$CHAINS"
+  --chain="$CHAINS" \
+  --env-file="$ENVFILE"
 
