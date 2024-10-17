31be1cc4e2c59d35acd92b66ac3400ea0adc7f15
olupy <fortuneosho@gmail.com>
2024-10-17 12:31:43 +0100

    unused parameters removed

--- backend/controllers/transaction.go
+++ backend/controllers/transaction.go
@@ -1029,7 +1029,7 @@ func handleOffRampTransaction(input serializers.MobileOffRamp, user models.User,
 		return
 	}
 
-	if err := storeHurupayRequest(input, user, output, resp.Data.PayoutRequestID); err != nil {
+	if err := storeHurupayRequest(input, user, resp.Data.PayoutRequestID); err != nil {
 		log.Error(err)
 	}
 }
@@ -1088,7 +1088,7 @@ func prepareTransactionDetails(input serializers.MobileOffRamp, resp apis.Payout
 }
 
 // Store the Hurupay request to the database
-func storeHurupayRequest(input serializers.MobileOffRamp, user models.User, output apis.MobilePayoutResponse, requestId string) error {
+func storeHurupayRequest(input serializers.MobileOffRamp, user models.User, requestId string) error {
 	request := models.HurupayRequest{
 		Amount:          input.AmountSending,
 		UserId:          int32(user.ID),
