9e93c4e4f86dc6c16df5a45c2b19f26dacf3d76d
WillDera <godswillezeoke@gmail.com>
2025-04-16 17:49:46 +0100

    Update allowed hosts

--- backend/middlewares/allowedhost.go
+++ backend/middlewares/allowedhost.go
@@ -3,16 +3,16 @@ package middlewares
 import (
 	"net/http"
 
+	"slices"
+
 	"github.com/gin-gonic/gin"
 )
 
 func AllowedHosts(allowedHosts []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		requestHost := c.Request.Host
-		for _, allowedHost := range allowedHosts {
-			if requestHost == allowedHost {
-				return
-			}
+		if slices.Contains(allowedHosts, requestHost) {
+			return
 		}
 
 		// If the host is not allowed, return a 403 Forbidden response
